7bd0dbe0a2f263b8cf92ea03b15bf40a15a27d15
MuditRajput2001 <you@example.commuditrajputpersonal@gmail>
2024-05-05 23:06:19 +0530

    'addPdfBug'

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import { numberOfPdf, singlePdfInfo, uploadPdfController } from "../controllers/admin.controller.js";
-import { adminVerify, isUserAdmin } from "../middleware/admin.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 const router = Router();
 
 // Your existing routes
-router.route('/addPdf').post(adminVerify, upload.single('pdf'), uploadPdfController);
+router.route('/addPdf').post( upload.single('pdf'), uploadPdfController);
 router.route('/numberOfPdf').get(numberOfPdf)
 router.route('/singlePdf/:name').get(singlePdfInfo)
-router.route('/auth').post(adminVerify, isUserAdmin, (req, res) => res.status(200).send({ success: true }));
+// router.route('/auth').post(adminVerify, isUserAdmin, (req, res) => res.status(200).send({ success: true }));
 
 // Add the sitemap route
 
